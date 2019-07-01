e80d33b43b7fc3e98a67ffdb93012239a5259096
T. Lanette <TLPollard4516@gmail.com>
2019-07-01 19:13:25 -0400

    Update feedreader.js
    Completed test for hidden menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,11 +50,13 @@ $(function() {
                 expect(Feed.name.length).not.toBe(0);
             });
          });
-         
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+
+    describe("The menu", function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -62,13 +64,24 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-         /* TODO: Write a test that ensures the menu changes
+         it("is hidden by default", function() {
+            const checkForClass = $("body").hasClass("menu-hidden");
+            expect(checkForClass).toBe(true);
+         });
+
+          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+    });
+
+        
+
     /* TODO: Write a new test suite named "Initial Entries" */
+
+    describe("Initial Entries", function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -77,10 +90,17 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+    });
+        
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe("New Feed Selection", function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
+        
 }());
